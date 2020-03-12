83455a721f266ed9a6194481d50d5ab1222a0798
Pranjal Rane <rane.pranjal18@vit.edu>
2020-03-12 19:47:29 +0530

    skeleton added

---
+++ DS/skeleton.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+typedef struct node
+{
+    int flight_number;
+    char flight_name[20];
+    int price;
+    char time[10];
+    char date[20];
+    char from[20];
+    char to[20];
+
+    struct node *left, *right; 
+    int height;
+}node;
+
+typedef struct list
+{
+    struct node* data;
+    struct list* next;
+}list;
+
+//functions for insert 
+
+node* search (char from[20], char to[20], char date[20], node *root)\
+{
+
+}
+
+void display (list *l)
+{
+
+}
+
+int main()
+{
+    
+}
