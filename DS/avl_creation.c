f04262410b5da80c743fc5f33b4523dc2978a4a4
Pranjal Rane <rane.pranjal18@vit.edu>
2020-03-12 23:12:08 +0530

    insertion completed

--- DS/avl_creation.c
+++ DS/avl_creation.c
@@ -7,10 +7,6 @@ typedef struct Node{
     int height; 
 }Node; 
   
-// A utility function to get maximum of two integers 
-int max(int a, int b); 
-  
-// A utility function to get the height of the tree 
 int height(Node *N) 
 { 
     if (N == NULL) 
@@ -18,18 +14,14 @@ int height(Node *N)
     return N->height; 
 } 
   
-// A utility function to get maximum of two integers 
 int max(int a, int b) 
 { 
     return (a > b)? a : b; 
 } 
   
-/* Helper function that allocates a new node with the given key and 
-    NULL left and right pointers. */
 Node* newNode(int key) 
 { 
-    Node* node = (Node*) 
-                        malloc(sizeof(Node)); 
+    Node* node = (Node*) malloc(sizeof(Node)); 
     node->key   = key; 
     node->left   = NULL; 
     node->right  = NULL; 
@@ -37,55 +29,42 @@ Node* newNode(int key)
     return(node); 
 } 
   
-// A utility function to right rotate subtree rooted with y 
-// See the diagram given above. 
+
 Node *rightRotate(Node *y) 
 { 
     Node *x = y->left; 
     Node *temp = x->right; 
   
-    // Perform rotation 
     x->right = y; 
     y->left = temp; 
   
-    // Update heights 
     y->height = max(height(y->left), height(y->right))+1; 
     x->height = max(height(x->left), height(x->right))+1; 
-  
-    // Return new root 
+
     return x; 
 } 
-  
-// A utility function to left rotate subtree rooted with x 
-// See the diagram given above. 
+   
 Node *leftRotate(Node *x) 
 { 
     Node *y = x->right; 
     Node *temp = y->left; 
-  
-    // Perform rotation 
+
     y->left = x; 
     x->right = temp; 
-  
-    //  Update heights 
+
     x->height = max(height(x->left), height(x->right))+1; 
     y->height = max(height(y->left), height(y->right))+1; 
-  
-    // Return new root 
+ 
     return y; 
 } 
   
-// Get Balance factor of node N 
 int getBalance(Node *N) { 
     if (N == NULL) 
         return 0; 
     return height(N->left) - height(N->right); 
 } 
-  
-// Recursive function to insert a key in the subtree rooted 
-// with node and returns the new root of the subtree. 
+
 Node* insert(Node* node, int key) { 
-    /* 1.  Perform the normal BST insertion */
     if (node == NULL) 
         return(newNode(key)); 
   
@@ -93,42 +72,29 @@ Node* insert(Node* node, int key) {
         node->left  = insert(node->left, key); 
     else if (key > node->key) 
         node->right = insert(node->right, key); 
-    else // Equal keys are not allowed in BST 
+    else 
         return node; 
-  
-    /* 2. Update height of this ancestor node */
-    node->height = 1 + max(height(node->left), 
-                           height(node->right)); 
-  
-    /* 3. Get the balance factor of this ancestor 
-          node to check whether this node became 
-          unbalanced */
+
+    node->height = 1 + max(height(node->left), height(node->right)); 
+                    
     int balance = getBalance(node); 
-  
-    // If this node becomes unbalanced, then 
-    // there are 4 cases 
-  
-    // Left Left Case 
+e 
     if (balance > 1 && key < node->left->key) 
         return rightRotate(node); 
-  
-    // Right Right Case 
+
     if (balance < -1 && key > node->right->key) 
         return leftRotate(node); 
-  
-    // Left Right Case 
+
     if (balance > 1 && key > node->left->key) { 
         node->left =  leftRotate(node->left); 
         return rightRotate(node); 
     } 
-  
-    // Right Left Case 
+
     if (balance < -1 && key < node->right->key) { 
         node->right = rightRotate(node->right); 
         return leftRotate(node); 
     } 
-  
-    /* return the (unchanged) node pointer */
+
     return node; 
 } 
 void preOrder(Node *root) { 
@@ -138,107 +104,7 @@ void preOrder(Node *root) {
         preOrder(root->right); 
     } 
 } 
-Node * minValueNode(Node* node) 
-{ 
-    Node* current = node; 
-  
-    /* loop down to find the leftmost leaf */
-    while (current->left != NULL) 
-        current = current->left; 
-  
-    return current; 
-} 
 
-Node* deleteNode(Node* root, int key) 
-{ 
-    // STEP 1: PERFORM STANDARD BST DELETE 
-  
-    if (root == NULL) 
-        return root; 
-  
-    // If the key to be deleted is smaller than the 
-    // root's key, then it lies in left subtree 
-    if ( key < root->key ) 
-        root->left = deleteNode(root->left, key); 
-  
-    // If the key to be deleted is greater than the 
-    // root's key, then it lies in right subtree 
-    else if( key > root->key ) 
-        root->right = deleteNode(root->right, key); 
-  
-    // if key is same as root's key, then This is 
-    // the node to be deleted 
-    else
-    { 
-        // node with only one child or no child 
-        if( (root->left == NULL) || (root->right == NULL) ) 
-        { 
-            struct Node *temp = root->left ? root->left : 
-                                             root->right; 
-  
-            // No child case 
-            if (temp == NULL) 
-            { 
-                temp = root; 
-                root = NULL; 
-            } 
-            else // One child case 
-             *root = *temp; // Copy the contents of 
-                            // the non-empty child 
-            free(temp); 
-        } 
-        else
-        { 
-            // node with two children: Get the inorder 
-            // successor (smallest in the right subtree) 
-            struct Node* temp = minValueNode(root->right); 
-  
-            // Copy the inorder successor's data to this node 
-            root->key = temp->key; 
-  
-            // Delete the inorder successor 
-            root->right = deleteNode(root->right, temp->key); 
-        } 
-    } 
-  
-    // If the tree had only one node then return 
-    if (root == NULL) 
-      return root; 
-  
-    // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE 
-    root->height = 1 + max(height(root->left), 
-                           height(root->right)); 
-  
-    // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to 
-    // check whether this node became unbalanced) 
-    int balance = getBalance(root); 
-  
-    // If this node becomes unbalanced, then there are 4 cases 
-  
-    // Left Left Case 
-    if (balance > 1 && getBalance(root->left) >= 0) 
-        return rightRotate(root); 
-  
-    // Left Right Case 
-    if (balance > 1 && getBalance(root->left) < 0) 
-    { 
-        root->left =  leftRotate(root->left); 
-        return rightRotate(root); 
-    } 
-  
-    // Right Right Case 
-    if (balance < -1 && getBalance(root->right) <= 0) 
-        return leftRotate(root); 
-  
-    // Right Left Case 
-    if (balance < -1 && getBalance(root->right) > 0) 
-    { 
-        root->right = rightRotate(root->right); 
-        return leftRotate(root); 
-    } 
-  
-    return root; 
-} 
   
 int main() { 
   Node *root = NULL; 
--- DS/main.c
+++ DS/main.c
@@ -1,6 +1,165 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+typedef struct Node
+{
+    int flight_number;
+    /* char flight_name[20];
+    int price;
+    char time[10]; */
+    char date[20];
+    char from[20];
+    char dst[20];
+
+    struct Node *left, *right; 
+    int height;
+}Node;
+
+
+Node* newNode(int key, char a[],char b[],char c[]) 
+{ 
+    Node* node = (Node*) malloc(sizeof(Node)); 
+    node->flight_number   = key; 
+    strcpy(node->date,a);
+    strcpy(node->from,b);
+    strcpy(node->dst,c);
+    node->left   = NULL; 
+    node->right  = NULL; 
+    node->height = 1; 
+
+    return(node); 
+}
+
+int height(Node *N) 
+{ 
+    if (N == NULL) 
+        return 0; 
+
+    return N->height; 
+} 
+  
+int max(int a, int b) 
+{ 
+    return (a > b)? a : b; 
+} 
+
+Node *rightRotate(Node *y) 
+{ 
+    Node *x = y->left; 
+    Node *temp = x->right; 
+  
+    x->right = y; 
+    y->left = temp; 
+  
+    y->height = max(height(y->left), height(y->right))+1; 
+    x->height = max(height(x->left), height(x->right))+1; 
+
+    return x; 
+} 
+   
+Node *leftRotate(Node *x) 
+{ 
+    Node *y = x->right; 
+    Node *temp = y->left; 
+
+    y->left = x; 
+    x->right = temp; 
+
+    x->height = max(height(x->left), height(x->right))+1; 
+    y->height = max(height(y->left), height(y->right))+1; 
+ 
+    return y; 
+} 
+  
+int getBalance(Node *N) 
+{ 
+    if (N == NULL) 
+        return 0; 
+
+    return height(N->left) - height(N->right); 
+} 
+
+Node* insert(Node* node, int key, char a[],char b[],char c[]) 
+{ 
+    if (node == NULL) 
+        return(newNode(key,a,b,c)); 
+  
+    if (key < node->flight_number) 
+        node->left  = insert(node->left, key,a,b,c); 
+    else if (key > node->flight_number) 
+        node->right = insert(node->right, key, a,b,c); 
+    else
+        return node; 
+  
+    node->height = 1 + max(height(node->left), height(node->right)); 
+    int balance = getBalance(node); 
+
+    if (balance > 1 && key < node->left->flight_number) 
+        return rightRotate(node); 
+
+    if (balance < -1 && key > node->right->flight_number) 
+        return leftRotate(node); 
+
+    if (balance > 1 && key > node->left->flight_number) 
+    { 
+        node->left =  leftRotate(node->left); 
+        return rightRotate(node); 
+    } 
+
+    if (balance < -1 && key < node->right->flight_number) 
+    { 
+        node->right = rightRotate(node->right); 
+        return leftRotate(node); 
+    } 
+
+    return node; 
+} 
+
+void preOrder(Node *root) 
+{ 
+    if(root != NULL) 
+    { 
+        printf("%d ", root->flight_number); 
+        preOrder(root->left); 
+        preOrder(root->right); 
+    } 
+} 
 
 int main()
 {
+    int c, n;
+    char date[20], from[20], to[20];
+    Node *root;
+    //root = new = (node*) malloc(sizeof(node));
+    root = NULL;
+
+    int flight_number;
+
+    printf ("1. Add Flight Data \n2. Book Flights \n3. Exit\n");
+    scanf ("%d", &c); 
+    
+    switch (c)
+    {
+        case 1: printf ("Enter number of flights to be added : ");
+                scanf ("%d", &n);
+
+                for (int i = 0; i < n; i++)
+                {
+                    printf ("1");
+                    printf ("Enter flight number : ");
+                    scanf ("%d", &flight_number);
+                    printf ("Enter date : ");
+                    scanf ("%s", date);
+                    printf ("Enter departure city : ");
+                    scanf ("%s", from);
+                    printf ("Enter Arrival city : ");
+                    scanf ("%s", to);
+
+                    root = insert (root, flight_number, date, from, to);
+                }
+                printf ("\npreorder : ");
+                preOrder (root);
+    }
     return 0;
 }
